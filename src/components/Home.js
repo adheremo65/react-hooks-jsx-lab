cb51737abd8cac94798fcd37f17718ab9e69c04d
Abdela umer <adheremo65@gmail.com>
2023-05-21 16:27:37 -0400

    react-hooks-jsx add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
+
+
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  // const colorStyle = {
+  //   color: 'firebrick'
+  // }
+  return <div id="home" >
+    <h1 style={{color:"firebrick"}}> {name} is a Web Developer from  {city}</h1>
+    Home 
+    </div>
 }
 
 export default Home;
